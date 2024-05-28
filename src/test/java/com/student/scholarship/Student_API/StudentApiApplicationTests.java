9ef8d079ac1083758e30d712bb2fbfed44d0006d
Pratik <pratikyadav3949@gmail.com>
2024-05-28 11:37:29 +0530

    project Created

---
+++ src/test/java/com/student/scholarship/Student_API/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.scholarship.Student_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
